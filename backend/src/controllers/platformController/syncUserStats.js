8b65613f371c8357f3aadda8608efe7f2c34a91a
adityapratap2005 <adityapratap1703@gmail.com>
2025-12-26 11:21:06 +0530

    Error Resolve in syncDashBoard

--- backend/src/controllers/platformController/syncUserStats.js
+++ backend/src/controllers/platformController/syncUserStats.js
@@ -2,12 +2,12 @@ import UserModel from "../../models/userModel.js"
 import UserPlatform from "../../models/userPlatform.js";
 //import LeetCodeProfile from "../../models/LeetCodeProfile.js";
 
-import { codeforcesQueue } from "../../queues/cf.queue.js";
+import { addCodeforcesSyncJob } from "../../queues/cf.queue.js";
 import { syncLeetCode } from "../../services/leetcode/leetcodeSync.js";
 
 export const syncDashboard = async (req, res) => {
   try {
-    const userId = req.userId;
+    const {userId} = req.body;
 
     // 1. Fetch user & handles
     const user = await UserModel.findById(userId).lean();
@@ -16,14 +16,15 @@ export const syncDashboard = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    const { codeforcesHandle, leetcodeHandle } = user;
+    const codeforcesHandle = await UserPlatform.findOne({user:userId,platform:"Codeforces"});
+    const leetcodeHandle = await UserPlatform.findOne({user:userId,platform:"LeetCode"});
 
     // 2. Trigger Codeforces sync (ASYNC)
     if (codeforcesHandle) {
-      await codeforcesQueue({
+      await addCodeforcesSyncJob({
         type: "NORMAL_SYNC",
         userId,
-        handle: codeforcesHandle,
+        handle: codeforcesHandle.username,
       });
     }
 
@@ -31,14 +32,14 @@ export const syncDashboard = async (req, res) => {
     if (leetcodeHandle) {
       await syncLeetCode({
         userId,
-        handle: leetcodeHandle,
+        handle: leetcodeHandle.username,
       });
     }
 
     // 4. Fetch latest known data from DB
     const [cfProfile, lcProfile] = await Promise.all([
       UserPlatform.findOne({ user:userId,platform:"Codeforces" }).lean(),
-      UserPlatform.findOne({ user:userId,platform:"Leetcode" }).lean(),
+      UserPlatform.findOne({ user:userId,platform:"LeetCode" }).lean(),
     ]);
 
     // 5. Respond immediately
